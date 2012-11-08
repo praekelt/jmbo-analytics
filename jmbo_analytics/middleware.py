af6e932b71bb5f608247c540fb4cde3ba3bf0034
Milton <milton@praekeltfoundation.org>
2012-11-08 15:39:30 +0200

    dont return anything

--- jmbo_analytics/middleware.py
+++ jmbo_analytics/middleware.py
@@ -4,4 +4,4 @@ from jmbo_analytics.tasks import send_ga_tracking
 
 class GoogleAnalyticsMiddleware(object):
     def process_request(self, request):
-        return send_ga_tracking.delay(build_ga_params(request))
+        send_ga_tracking.delay(build_ga_params(request))
